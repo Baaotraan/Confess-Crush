bc997b9ea1af840a7a383948898ee978e7696ecb
Baaotraan <94889722+Baaotraan@users.noreply.github.com>
2021-11-23 14:36:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,17 @@ const CONFIG = {
     titleWeb: "mie lò vé",
     introTitle: 'Huyynh Phuoong',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao tuiii cứ đi nhầm đường
-    Lạc vào tim Phuông lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao tay tuiii lại xấu thế này`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã muốn đi làm nail từ sau cái giản cách xã hội 🥰',
-    desc: 'Phải chăng Phuông đã khoái khoái chảy nước miếng chảy nước miếng ',
+    desc: 'khoái khoái, thèm chảy nước miếng, chảy nước miếng ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người sao lại chưa đi làm nail nữa :>> ',
     btnReply: 'Gửi cho Baaotraan <3',
     reply: 'Đi thì đi mà không đi thì đi <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, qua đón mình đi chơi nha.',
+    messDesc: 'Tối cn 5h, qua đón mình đi làm nail dùm cái.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100015339345019' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
